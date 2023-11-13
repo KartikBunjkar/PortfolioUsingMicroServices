4362c1ea2335283c8eba98558f354bdc8e546d09
KartikBunjkar <kartikbunjkar@gmail.com>
2023-11-13 17:42:21 +0530

    third commit

--- portfolio/src/main/java/com/kartikbunjkar/portfolio/domain/Achievements.java
+++ portfolio/src/main/java/com/kartikbunjkar/portfolio/domain/Achievements.java
@@ -1,5 +1,72 @@
 package com.kartikbunjkar.portfolio.domain;
 
-public class Achievements {
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity (name = "achievements")
+
+public class Achievements {
+	
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column (name = "title")
+	private String title;
+	
+	@Column (name = "description")
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Achievements other = (Achievements) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(title, other.title);
+	}
+
+	@Override
+	public String toString() {
+		return "Achievements [id=" + id + ", title=" + title + ", description=" + description + "]";
+	}
+	
+	
+	
 }
--- portfolio/src/main/java/com/kartikbunjkar/portfolio/domain/Technologies.java
+++ portfolio/src/main/java/com/kartikbunjkar/portfolio/domain/Technologies.java
@@ -1,5 +1,70 @@
 package com.kartikbunjkar.portfolio.domain;
 
-public class Technologies {
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity (name = "technologies")
+public class Technologies {
+	
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column (name = "name")
+	private String name;
+	
+	@Column (name = "description")
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Technologies other = (Technologies) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Technologies [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	
+	
 }
